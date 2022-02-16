71cbcbe07f32a4139b223a7da41e0db0ef904868
galib-codiner <galib@codincity.com>
2022-02-17 01:19:18 +0530

    designed pages

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,13 +1,16 @@
 import React from 'react'; 
+import './Address.css';
 
 const Address = ({location}) => {
     return (
         <div className="address">
-            <h4>Address</h4>
-                <p> <span> Street: </span>{location.street.number} {location.street.name} </p>
-                <p> <span> City: </span>{location.city}  </p>
-                <p> <span> Country: </span>{location.country}  </p>
-                <p> <span> Postcode: </span>{location.postcode}  </p>
+            <h4>Address :- </h4>
+                <pre>
+                <p> <span> Street : </span>{location.street.number} {location.street.name} </p>
+                <p> <span> City : </span>{location.city}  </p>
+                <p> <span> Country : </span>{location.country}  </p>
+                <p> <span> Postcode : </span>{location.postcode}  </p>
+            </pre>
         </div>
     )
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -11,30 +11,31 @@ const Users = ({userData}) => {
     return (
         <div className="user">
             <div className="user__body">
-                {/* <div className="user__title">{userData.name.first} {userData.name.last}</div> */}
                 <div>
                     <a className="user__title" onClick={() => setMOdalOpen(true)}>{userData.name.first} {userData.name.last}</a>
                     <Modal isOpen={modalOpen}>
                     <div>
                         <h3>Other Details</h3>
-                        <h4>Full Name: {userData.name.first} {userData.name.last}</h4>
-                        <p>Gender: {userData.gender}</p>
-                        <p>Email: {userData.email}</p>
-                        <p>Phone: {userData.phone}</p>
+                        <h4>Full Name : {userData.name.first} {userData.name.last}</h4>
+                        <p>Gender : {userData.gender}</p>
+                        <p>Email : {userData.email}</p>
+                        <p>Phone : {userData.phone}</p>
                     </div>
                     <div>
                         <button onClick={() => setMOdalOpen(false)} >Close</button>
                     </div>
                     </Modal>
                 </div>
-                <div> <span>Username</span> {userData.login.username}</div>
-                <div> <span>Email</span> {userData.email}</div>
-                <div> <span>DOB</span> {userData.dob.date}</div>
+                
+                <div className='other-details'>
+                    <p><span>Username : </span> {userData.login.username}</p>
+                    <p><span>Email : </span> {userData.email}</p>
+                    <p><span>DOB : </span> {userData.dob.date}</p>
+                </div>
                 
                 <Address location={userData.location} />
-                <div> <span>Phone</span> {userData.phone}</div>
-                {/* <div className="user__image"><img src={userData.picture.medium}/></div> */}
-                <a className="user__title" onClick={() => setImageOpen(true)}><img src={userData.picture.thumbnail}/></a>
+                <div className='phone'> <span>Phone : </span> {userData.phone}</div>
+                <a className="user__image" onClick={() => setImageOpen(true)}><img src={userData.picture.thumbnail}/></a>
                 <Modal isOpen={imageOpen}>
                     <div>
                     <img className='zoomImage' src={userData.picture.thumbnail} />
